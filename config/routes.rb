68038735d5bf758b527e0da750ef78e25e785bf2
alvinkenyagah <alvin47k@gmail.com>
2023-06-25 13:25:37 +0300

    completed assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :toys, only: [:index, :create, :update]
+  resources :toys, only: [:index, :create, :update, :destroy] #ADD DESTROY ROUTE: ActionController::RoutingError (No route matches [DELETE] "/toys/11"):
 end
